3e2e12f2e9e6e954f616ff3ae582015569e877e0
deqiying <deqiying@gmail.com>
2024-07-14 14:23:49 +0800

    update: 更新自定义规则

--- owngeo/download_ruleset.sh
+++ owngeo/download_ruleset.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# 获取脚本所在目录
+SCRIPT_DIR=$(dirname "$0")
+
+# 切换到脚本所在目录
+cd "$SCRIPT_DIR" || exit
+
 # 检查 ruleset_list.list 文件是否存在
 if [ ! -f ruleset_list.list ]; then
     echo "ruleset_list.txt 文件不存在"
