2d134799286578e2e7ce145c6254cb9807499785
deqiying <deqiying@gmail.com>
2024-07-14 14:13:17 +0800

    update: 更新自定义规则

--- owngeo/download_ruleset.sh
+++ owngeo/download_ruleset.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-# 检查 ruleset_list.txt 文件是否存在
-if [ ! -f ruleset_list.txt ]; then
+# 检查 ruleset_list.list 文件是否存在
+if [ ! -f ruleset_list.list ]; then
     echo "ruleset_list.txt 文件不存在"
     exit 1
 fi
 
-# 读取 ruleset_list.txt 并下载每个链接的文件
+# 读取 ruleset_list.list 并下载每个链接的文件
 while IFS= read -r url; do
     # 获取文件名
     filename=$(basename "$url")
 
     # 下载文件并覆盖同名文件
     wget -O "$filename" "$url"
-done < ruleset_list.txt
+done < ruleset_list.list
 
 echo "所有文件已下载并覆盖"
